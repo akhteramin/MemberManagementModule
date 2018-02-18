053391b31e6b38a4bb696e11520210ff28eb7f79
Jawad Iqbal <jawad.iqbal@ipay.com.bd>
2018-02-18 16:57:10 +0600

    updated introducer list api field names

--- src/components/MemberList/WaitingForVerificationListComponent.vue
+++ src/components/MemberList/WaitingForVerificationListComponent.vue
@@ -414,7 +414,7 @@
           this.showLoader = true
           Http.GET('member', [member.accountId, 'introducers'])
             .then(
-              ({data: {data: introducers}}) => {
+              ({data: {introducerList: introducers}}) => {
                 this.showLoader = false
                 this.memberIntroducers = introducers
                 console.log('Got the list of introducers: ', this.memberIntroducers)
