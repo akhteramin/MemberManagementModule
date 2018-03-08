b46f500293865faa671466b8b37d87ed490bbb08
Jawad Iqbal <jawad.iqbal@ipay.com.bd>
2018-03-08 11:40:09 +0600

    updated test config

--- src/services/Http.js
+++ src/services/Http.js
@@ -62,7 +62,7 @@ axios.interceptors.response.use(
 // const API_URL = 'http://10.10.10.169:8085/member-service/api/v1'
 // // const API_URL = 'http://10.10.10.199:8085/member-service/api/v1'
 // const MM_ADMIN_URL = 'http://10.10.10.10:9002/api/v1' // dev
-const MM_ADMIN_URL = 'http://10.15.40.10:9002/api/v1' // test
+const MM_ADMIN_URL = 'http://10.15.40.10:9001/api/v1' // test
 const AUTH_URL = 'http://10.10.40.31:8000/auth/api/v1' // test
 const AUTH_HTTP_URI = 'http://10.10.40.31:8080/'
 const AUTH_ACCOUNTS_URI = 'http://10.10.40.31:8080/accounts'
